fede51b9fe8aadb228269cae0578d1008911d6f2
DaniloToroL <danilo.toro@usach.cl>
2020-10-29 10:37:56 -0300

    update conf for mac

--- __init__.py
+++ __init__.py
@@ -54,7 +54,7 @@ class OdbcModule:
 
     @connection.setter
     def connection(self, value):
-        self.__connection = self.pyodbc.connect(value)
+        self.__connection = self.pyodbc.connect(**value)
 
 
 if module == "listDrivers":
@@ -71,7 +71,7 @@ if module == "listDrivers":
 
         SetVar(result, drivers)
     except Exception as e:
-        print("\x1B[" + "31;40mError\u2193\x1B[" + "0m")
+        print("\x1B[" + "31;40mError\x1B[" + "0m")
         PrintException()
         raise e
 
@@ -85,30 +85,67 @@ if module == "connect":
 
     try:
 
-        driver = "{" + driver + "}"
-        params = "" if params is None else params
-        conn_str = f'Driver={driver};' + params
+        args = {"driver": driver}
+        if params.startswith("{"):
+            params = eval(params)
+            for key in params:
+                args[key] = params[key]
+        elif params is not None or params != "":
+            for param in params.split(";"):
+                key_value = param.split("=")
+                args[key_value[0]] = key_value[1]
 
-        odbc_mod = OdbcModule(conn_str, pyodbc)
+        odbc_mod = OdbcModule(args, pyodbc)
 
     except Exception as e:
-        print("\x1B[" + "31;40mError\u2193\x1B[" + "0m")
+        print("\x1B[" + "31;40mError\x1B[" + "0m")
         PrintException()
         raise e
 
 if module == "execute_query":
+
+    def query2params(*args):
+        queries = []
+        print(args)
+        for param in args:
+            if param.startswith("to_date"):
+                p = datetime.strptime(date_, format_)
+                queries.append(p)
+            else:
+                queries.append(param)
+        return tuple(queries)
     try:
         query = GetParams('query')
         result = GetParams('var_')
+        params = GetParams('params')
+        cursor = conn.cursor()
 
-        cursor = odbc_mod.connection.cursor()
-        cursor.execute(query)
+        if query.lower().startswith(('{call', '{ call')):
+            params = tuple(params.split(","))
+            q = (query, ) + query2params(*params)
+            cursor.execute(*q)
+        else:
+            cursor.execute(query)
 
-        fetch = cursor.fetchall()
-        if result:
-            SetVar(result, fetch)
-
+        if query.lower().startswith(('select', '{call', '{ call')):
+            row = cursor.fetchone()
+            if row:
+                print(row[0])
+                # data = []
+            # # print(query)
+            # # data.append(columns)
+            # for row in cursor.fetchall():
+            #     # print(row)
+            #     ob_ = []
+            #     for r in row:
+            #         ob_.append(str(r) + "")
+            #     data.append(ob_)
+        else:
+            conn.commit()
+            data = cursor.rowcount, 'registros afectados'
+        conn.commit()
+        SetVar(result, data)
     except Exception as e:
-        print("\x1B[" + "31;40mError\u2193\x1B[" + "0m")
+        print("\x1B[" + "31;40mError\x1B[" + "0m")
         PrintException()
         raise e
